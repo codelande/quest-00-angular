e4438216521e7e1f9130ff36177db995aa8fa137
codelande <co.delande@yahoo.fr>
2023-01-16 11:26:24 +0100

    finish form

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent {
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+    showAge: true,
+  };
+
+  changeAgeView(): void {
+    this.user.showAge = !this.user.showAge;
+    console.log('tests');
+  }
+}
